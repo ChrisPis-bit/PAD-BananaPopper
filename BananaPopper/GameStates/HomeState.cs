203b52b6806038e6bca0618829f58f4bf3942836
Stormtrooper241 <masterdeadshot1987@gmail.com>
2020-03-06 12:55:58 +0100

    test commit

---
+++ BananaPopper/GameStates/HomeState.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BananaPopper.GameStates
+{
+    class HomeState:GameObject
+    {
+
+       Texture2D mouse = new Texture2D(GameEnvironment.Graphics.GraphicsDevice, 10, 10); //temporary texture for mouse
+
+
+    void HomeScreen()
+        {
+            SpriteGameObject theMouse = new SpriteGameObject(mouse);
+            //Add GameObjects here
+            Add(theMouse);
+
+
+        }
+
+    void StartButton()
+        {
+
+        }
+
+
+
+
+
+        private void Add(SpriteGameObject theMouse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
