74d610bb136a64a34b87cb5d1a2d33796f4b03ea
Brcns <56611001+Brcns@users.noreply.github.com>
2020-05-06 15:21:58 +0200

    Achtergrond muziek

--- BananaPopper/BananaPopper.cs
+++ BananaPopper/BananaPopper.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Xna.Framework.Media;
+
 namespace BananaPopper
 {
     class BananaPopper : GameEnvironment
@@ -12,6 +14,8 @@ namespace BananaPopper
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
         /// </summary>
+        /// 
+         Song backgroundMusic;
         protected override void LoadContent()
         {
            
@@ -19,6 +23,10 @@ namespace BananaPopper
             screen.X = 1600;
             screen.Y = 900;
             ApplyResolutionSettings();
+
+            backgroundMusic = Content.Load<Song>("MonkeyIslandBand");
+            MediaPlayer.IsRepeating = true;
+            MediaPlayer.Play(backgroundMusic);
 
             //Use globalScale on object pos and size to make it change in scale with the screen width/height
             globalScale = screen.X / 20;
