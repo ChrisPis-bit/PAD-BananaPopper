3624b605bf263220daea7b96359bc50041425c4e
ChrisPis-bit <54893471+ChrisPis-bit@users.noreply.github.com>
2020-05-07 20:27:36 +0200

    particle classes added

---
+++ BananaPopper/GameManagement/ParticleObject.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ParticleObject : SpriteGameObject
+{
+    protected int fadeTime; //Fade time defines how many frames the particle will be on screen
+
+    protected float gravity;
+
+    public ParticleObject(string assetname, Vector2 spawnPosition, Vector2 velocity, int fadeTime, float gravity = 0) : base(assetname)
+    {
+        this.fadeTime = fadeTime;
+        this.gravity = gravity;
+        position = spawnPosition;
+        this.velocity = velocity;
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+        fadeTime--;
+
+        if (fadeTime < 0) Visible = false;
+
+        velocity.Y += gravity;
+    }
+}
+
---
+++ BananaPopper/GameManagement/ParticleObjectList.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ParticleObjectList : GameObjectList
+{
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+
+        for (int iParticle = 0; iParticle < Children.Count(); iParticle++)
+        {
+            //Removes invisible particles
+            if (!Children[iParticle].Visible)
+            {
+                Children.RemoveAt(iParticle);
+                iParticle--;
+            }
+        }
+    }
+
+    //Places particles in the list with the filled in paramaters
+    //The max velocity defines the maximum speed the particle can have once it spawns
+    //The direction of the particle is randomized
+    public void SpawnSpriteParticles(string assetname, Vector2 spawnPosition, Vector2 maxVelocity, int particleAmount, int fadeTime, float gravity = 0)
+    {
+        for (int iParticle = 0; iParticle < particleAmount; iParticle++)
+        {
+            Add(new ParticleObject(assetname, spawnPosition, new Vector2(GameEnvironment.Random.Next((int)-maxVelocity.X, (int)maxVelocity.X), GameEnvironment.Random.Next((int)-maxVelocity.Y, (int)maxVelocity.Y)),
+                fadeTime, gravity));
+        }
+    }
+}
+
